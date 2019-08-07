7a7ee598e519525dc12150986272f4e333d806ab
Socio-path <doohee006@gmail.com>
2019-08-08 01:26:36 +0900

    Update version_downloader.py

--- version_downloader.py
+++ version_downloader.py
@@ -3,10 +3,6 @@ import json
 import os
 import requests
 
-def move(MC_version):
-    os.mkdir(MC_version)
-    os.chdir(MC_version)
-
 def L_Json_download(MC_version):
     url = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
     jsons = requests.get(url).json()['versions']
@@ -22,7 +18,8 @@ def L_Jar_download(MC_version,mcjsons):
     os.rename("client.jar", MC_version+".jar")
 
 def L_download(MC_version):
-    move(MC_version)
+    os.mkdir(MC_version)
+    os.chdir(MC_version)
     L_Json_download(MC_version)
 
 if __name__=="__main__":
